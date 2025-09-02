0547ba4228300267ed2c16da37c137303884003e
sami-elt <139684759+sami-elt@users.noreply.github.com>
2025-09-02 12:08:41 +0200

    .

--- hangManApp/ConsoleApp1/Program.cs
+++ hangManApp/ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        String secretWord = "spel";
+        int lifeLeft = 5;
+
+        StringBuilder displayString = new StringBuilder();
+
+        for (int i = 0; i < secretWord.Length; i++) {
+
+            displayString.Append('_');
+
+        }
+
+        Console.WriteLine(displayString);
+
+    }
+
+    
+
+
+
+
+}
