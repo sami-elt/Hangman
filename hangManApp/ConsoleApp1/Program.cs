b3aa69e07c628355130c531e82a214eb3f302e39
sami-elt <139684759+sami-elt@users.noreply.github.com>
2025-09-02 18:56:52 +0200

    working version

--- hangManApp/ConsoleApp1/Program.cs
+++ hangManApp/ConsoleApp1/Program.cs
@@ -1,33 +1,89 @@
-﻿
-
-
-
+﻿using System;
 using System.Text;
+
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Välkommen till hänga gubbe! \nDu har 5 liv på dig att gissa rätt ord :)");
 
-        String secretWord = "spel";
         int lifeLeft = 5;
+        String[] wordList = ["spel", "test", "huskur", "hangman"];
+
+        Random random = new Random();
+
+        string secretWord = wordList[random.Next(wordList.Length)];
 
         StringBuilder displayString = new StringBuilder();
 
-        for (int i = 0; i < secretWord.Length; i++) {
+        for (int i = 0; i < secretWord.Length; i++)
+        {
 
             displayString.Append('_');
 
         }
+        int secret = secretWord.Length;
 
-        Console.WriteLine(displayString);
+        while (lifeLeft > 0)
+        {
 
+            Console.WriteLine("Välj bokstav: ");
+            Console.WriteLine("Liv kvar: " + lifeLeft);
+
+            Console.WriteLine(displayString);
+
+            string guess = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(guess))
+            {
+
+                int i = 0;
+
+                bool found = false;
+
+                foreach (char letter in secretWord)
+                {
+                    if (letter == guess[0])
+                    {
+                        found = true;
+                        displayString[i] = letter;
+                        //Console.WriteLine("Rätt!");
+                        secret--;
+                    }
+
+                    i++;
+                }
+
+
+                if (secret == 0)
+                {
+                    Console.WriteLine("Du vann");
+                    break;
+                }
+
+
+                if (!found)
+                {
+                    lifeLeft--;
+                    Console.WriteLine("Fel!");
+                }
+
+ 
+
+            }
+
+            else
+            {
+                Console.WriteLine("får inte vara tomt!");
+            }
+
+        }
+
+
+ 
     }
 
-    
 
 
-
-
-}
+    }
